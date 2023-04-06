be9af3e3b4fa897a0d94f17e50895442b684d42b
vega005 <vega005@interia.pl>
2023-04-06 17:41:36 +0200

    Added new lines in Item and create new Category file.

---
+++ src/main/java/com/sklepinternetowy/internetshop/Category.java
@@ -0,0 +1,12 @@
+package com.sklepinternetowy.internetshop;
+
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+public class Category {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String name;
+}
--- src/main/java/com/sklepinternetowy/internetshop/model/Item.java
+++ src/main/java/com/sklepinternetowy/internetshop/model/Item.java
@@ -1,5 +1,6 @@
 package com.sklepinternetowy.internetshop.model;
 
+import com.sklepinternetowy.internetshop.Category;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
@@ -20,7 +21,13 @@ public class Item {
     @GeneratedValue
     private Long id;
     private String name;
+    private String description;
     private BigDecimal price;
+    private BigDecimal min_price;
+    private Boolean used;
+    private int available;
+    private float discount;
+    private Category category;
     private String imgUrl;
 
     public Item(String name, BigDecimal price, String imgUrl) {
@@ -28,4 +35,18 @@ public class Item {
         this.price = price;
         this.imgUrl = imgUrl;
     }
+
+
+//    public Item(Long id, String name, String description, BigDecimal price, BigDecimal min_price, Boolean used, int available, float discount, Category category, String imgUrl) {
+//        this.id = id;
+//        this.name = name;
+//        this.description = description;
+//        this.price = price;
+//        this.min_price = min_price;
+//        this.used = used;
+//        this.available = available;
+//        this.discount = discount;
+//        this.category = category;
+//        this.imgUrl = imgUrl;
+//    }
 }
